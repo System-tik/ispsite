211aec4f7e8729bb0bb26a9a3ac1502a47f96348
elshammar <kanyindamampova@gmail.com>
2024-01-30 02:45:10 +0400

    add register

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
