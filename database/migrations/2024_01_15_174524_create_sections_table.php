5ee769b981ce09367fd43a23418b84530d38cfce
elshammar <kanyindamampova@gmail.com>
2024-01-30 14:48:21 +0400

    section updated

--- database/migrations/2024_01_15_174524_create_sections_table.php
+++ database/migrations/2024_01_15_174524_create_sections_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
             $table->string('lib');
+            $table->string('description');
             $table->timestamps();
         });
     }
